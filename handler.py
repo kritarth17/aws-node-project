5a6a368ebac46ff83809cbdc2dfb8e1cf727635e
kritarth17 <akritarth@gmail.com>
2024-05-02 16:48:39 -0400

    updated serverless.yml and handler.py

--- handler.py
+++ handler.py
@@ -1,7 +1,17 @@
 import json
+import boto3
+from io import BytesIO
+from PIL import Image, ImageOps
+import os
+import uuid
+
+s3 = boto3.client('s3')
+size = int(os.environ['THUMBNAIL_SIZE'])
 
 
-def hello(event, context):
+def s3_thumbnail_generator(event, context):
+    #parse event
+    print("EVENT:::", event)
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
